9945dc0293705a04f5f2281c66ce7526aed8125e
Stas-0k <brickbybrick15@gmail.com>
2022-11-02 23:21:00 +0200

    commit
    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+
+console.log(Player)
+
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function(timeupdate) {
+        console.log(timeupdate);
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
 
