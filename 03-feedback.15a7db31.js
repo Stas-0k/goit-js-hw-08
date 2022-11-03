142e28d3123d98032f467522deeeecb0beb5ae4e
Stas-0k <brickbybrick15@gmail.com>
2022-11-03 22:01:27 +0000

    Deploying to gh-pages from @ Stas-0k/goit-js-hw-08@3b3093c7c70416bf4672632e48a99e1a216fbc82 🚀

---
+++ 03-feedback.15a7db31.js
@@ -0,0 +1,2 @@
+document.querySelector("input"),document.querySelector("textarea");document.querySelector(".feedback-form").addEventListener("input",(function(e){const t=({elements:{email:email,message:message}}=e.currentTarget);localStorage.setItem("feedback-form-state",t)}));
+//# sourceMappingURL=03-feedback.15a7db31.js.map
