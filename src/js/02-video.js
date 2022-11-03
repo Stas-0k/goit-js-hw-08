3b3093c7c70416bf4672632e48a99e1a216fbc82
Stas-0k <brickbybrick15@gmail.com>
2022-11-04 00:00:59 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,16 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function (timeupdate) {
+player.on('play', throttle(data, 1000));
+
+function data (timeupdate) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(timeupdate));    
-});
+}
 let stopTime = localStorage.getItem('videoplayer-current-time');
-let jsStopTime = JSON.parse(stopTime)
-jsStopTime.seconds = 0;
-    
+let jsStopTime = JSON.parse(stopTime)    
 player.setCurrentTime(jsStopTime.seconds)
 
 
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+const emailInput = document.querySelector('input')
+const messageInput = document.querySelector('textarea')
+const form = document.querySelector('.feedback-form')
 
+
+
+
+form.addEventListener('input', show)
+
+
+function show(event) { 
+    const result = { elements: { email, message } } = event.currentTarget;
+  
+    localStorage.setItem('feedback-form-state', result)
+}
