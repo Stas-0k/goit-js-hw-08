cc5a92e27db7134a83c35cbbcf8ada740f19de1d
Stas-0k <brickbybrick15@gmail.com>
2022-11-04 14:45:08 +0200

    commit
    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,42 @@
+import throttle from 'lodash.throttle'
+const formData = document.querySelector('.feedback-form')
 const emailInput = document.querySelector('input')
 const messageInput = document.querySelector('textarea')
-const form = document.querySelector('.feedback-form')
+
+let dataInput = {};
+
+
+dataPreview();
+
+formData.addEventListener('input', throttle(show, 500))
 
 
 
+function show(evt) { 
+        dataInput[evt.target.name]= evt.target.value,
+        dataInput[evt.target.name]= evt.target.value,    
+            
+    localStorage.setItem("feedback-form-state",
+    JSON.stringify(dataInput))
+}
 
-form.addEventListener('input', show)
 
+function dataPreview() { 
+    if (localStorage.getItem('feedback-form-state')) { 
+        const jsonData = localStorage.getItem('feedback-form-state')
+        const parseData = JSON.parse(jsonData)
+        emailInput.value = parseData.email;
+        messageInput.value = parseData.message;
+    }
+}
 
-function show(event) { 
-    const result = { elements: { email, message } } = event.currentTarget;
-  
-    localStorage.setItem('feedback-form-state', result)
+formData.addEventListener('submit', submitFunc)
+
+function submitFunc(evt) { 
+    evt.preventDefault(); 
+
+    localStorage.removeItem('feedback-form-state')
+    emailInput.value = ''
+    messageInput.value = ''
+    console.log(dataInput)
 }
