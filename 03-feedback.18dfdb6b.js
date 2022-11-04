5678c0ad2e61684fbf9e64331f7ea232a2bcc583
Stas-0k <brickbybrick15@gmail.com>
2022-11-04 12:45:48 +0000

    Deploying to gh-pages from @ Stas-0k/goit-js-hw-08@cc5a92e27db7134a83c35cbbcf8ada740f19de1d 🚀

--- 03-feedback.18dfdb6b.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("input"),document.querySelector("textarea");document.querySelector(".feedback-form").addEventListener("input",(function(e){var t,r,a=(t=e.currentTarget,r=t.elements,email=r.email,message=r.message,t);localStorage.setItem("feedback-form-state",a)}))}();
-//# sourceMappingURL=03-feedback.18dfdb6b.js.map
