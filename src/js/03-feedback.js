ebc7120b91c62b380d9a8b6680fb90a66dd38ba2
Stas-0k <brickbybrick15@gmail.com>
2022-11-07 00:30:59 +0200

    fixed 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,41 +3,33 @@ const formData = document.querySelector('.feedback-form')
 const emailInput = document.querySelector('input')
 const messageInput = document.querySelector('textarea')
 
-let dataInput = {};
+let dataInput = {email:"",message:""};
 
 
 dataPreview();
+
+
 
 formData.addEventListener('input', throttle(show, 500))
 
 
 
 function show(evt) { 
-        dataInput[evt.target.name]= evt.target.value,
-        dataInput[evt.target.name]= evt.target.value,    
+    dataInput[evt.target.name]= evt.target.value,
+    dataInput[evt.target.name]= evt.target.value,    
             
-    localStorage.setItem("feedback-form-state",
-    JSON.stringify(dataInput))
+    localStorage.setItem("feedback-form-state", JSON.stringify(dataInput))
 }
-
 
 function dataPreview() { 
     if (localStorage.getItem('feedback-form-state')) { 
         const jsonData = localStorage.getItem('feedback-form-state')
         const parseData = JSON.parse(jsonData)
-        if (parseData.email) {
-            emailInput.value = parseData.email;
-        } else { 
-            alert('All fields must be filled!')
-            emailInput.value = ''
-        }
-
-        if (parseData.message) {
-            messageInput.value = parseData.message;
-        } else { 
-            alert('All fields must be filled!')
-            messageInput.value = ''
-        }
+        emailInput.value = parseData.email;
+        messageInput.value = parseData.message;
+        dataInput.email = parseData.email;
+        dataInput.message = parseData.message;
+       
         
     }
 }
